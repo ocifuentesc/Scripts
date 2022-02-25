dc25b2a1991acc73f4c24b772990ecbc9d5c0b7c
ocifuentesc <oscarcifuentescisterna@gmail.com>
2022-02-25 02:41:33 -0300

    Add files via upload

---
+++ Mantenimiento_Debian.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# ███╗   ██╗███████╗████████╗███████╗███████╗ ██████╗██╗   ██╗██████╗ ██╗████████╗██╗   ██╗
+# ████╗  ██║██╔════╝╚══██╔══╝██╔════╝██╔════╝██╔════╝██║   ██║██╔══██╗██║╚══██╔══╝╚██╗ ██╔╝
+# ██╔██╗ ██║█████╗     ██║   ███████╗█████╗  ██║     ██║   ██║██████╔╝██║   ██║    ╚████╔╝ 
+# ██║╚██╗██║██╔══╝     ██║   ╚════██║██╔══╝  ██║     ██║   ██║██╔══██╗██║   ██║     ╚██╔╝  
+# ██║ ╚████║███████╗   ██║   ███████║███████╗╚██████╗╚██████╔╝██║  ██║██║   ██║      ██║   
+# ╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝   ╚═╝      ╚═╝   
+#
+# Sitio Web: https://netsecurity.cl                                                                                         
+
+echo -e "¿Desea realizar el mantenimiento al sistema? (S/N): \c"
+read MyVar1
+echo "Estimad@ $USER, Ha seleccionado $MyVar1"
+
+case $MyVar1 in
+        [Ss])
+                echo -e "¿Desea Actualizar el Sistema? (S/N): \c"
+                read MyVar2
+                if [ $MyVar2 == "S" ]
+                        then
+                                echo "Se procede a actualizar..."
+                                sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
+                fi
+                echo -e "¿Desea limpiar la memoria cache? (S/N): \c"
+                read MyVar3
+                if [ $MyVar3 == "S" ]
+                        then
+                                echo "Se procede a limpiar la memoria cache..."
+                                sudo apt autoremove -y && sudo apt autoclean
+                fi
+                echo -e "¿Desea Desocupar la Carpeta /tmp/? (S/N): \c"
+                read MyVar4
+                if [ $MyVar4 == "S" ]
+                        then
+                                echo "Se procede a desocupar la carpeta /tmp/..."
+                                sudo rm -r /tmp/*
+                                echo "Carpeta desocupada"
+                fi
+                echo "Mantenimiento Terminado"
+                exit 0
+                ;;
+        [Nn])
+                echo "Mantenimiento Cancelado"
+                exit 0
+                ;;
+        *)
+                echo -e "$USER, has seleccionado $MyVar1, esta es una opción inválida, se procede a cancelar la ejeccución del script"
+                exit 0
+                ;;
+esac
+
